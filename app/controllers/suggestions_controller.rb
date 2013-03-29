4936ba4b37c2729cc8579efa9e7cd1c588dd64a4
Nikos Gereoudakis <gereoudakis@gmail.com>
2013-03-29 21:32:22 +0200

    adds email notification on suggestion creation

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -5,6 +5,7 @@ class SuggestionsController < ApplicationController
     @proposal = Proposal.find(params[:proposal_id])
     @suggestion = current_user.suggestions.build(params[:suggestion].merge(:proposal => @proposal))
     if @suggestion.save
+      SuggestionsMailer.new_suggestion(@suggestion).deliver 
       redirect_to proposal_path(@proposal)
     else
       render :template => 'proposals/show'
