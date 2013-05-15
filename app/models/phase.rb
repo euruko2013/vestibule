33f168fbd481e5f1af0849a13626a2ef99c0b04a
Fotos Georgiadis <fotos@skroutz.gr>
2013-05-15 12:59:38 +0300

    Change LINEUP phase main text message
    Fixes #51.

--- app/models/phase.rb
+++ app/models/phase.rb
@@ -134,7 +134,7 @@ class Phase
 
   LINEUP = Phase.new.tap do |p|
     p.name = "Lineup announcement"
-    p.main_text = "That's all folks! The voting period has ended and soon the final speaker lineup will be announced. Until then, grab your ticket if you haven't already and see you in Athens!"
+    p.main_text = "That's all folks! The voting period has ended and soon the final speaker lineup has been announced. Grab your sunscreen, pack your bag, and see you in Athens!"
     p.starting_at = DateTime.parse('2013-05-09T00:00:00+3')
     p.ending_at = DateTime.parse('2100-01-01T00:00:00+2')
 
